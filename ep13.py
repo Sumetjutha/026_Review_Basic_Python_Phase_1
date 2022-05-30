664fcb93fe0f923fc3f9f47343d8ef01d839f020
Sumetjutha <sumet.jutha@gmail.com>
2022-05-31 02:30:43 +0700

    update

--- ep13.py
+++ ep13.py
@@ -1,6 +1,5 @@
 # โปรแกรมคำนวณค่า BMI (ดัชนีมวลกาย)
 # BMI = น้ำหนัก (kg) / ส่วนสูง * ส่วนสูง
-
 # Input / convert to integer
 weight = int(input("ป้อนน้ำหนักของคุณ (kg) :"))
 height = int(input("ป้อนส่วนสูงของคุณ (cm) :"))/100
