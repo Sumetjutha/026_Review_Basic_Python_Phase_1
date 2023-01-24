ca567c29320ede255631bdd41fa8d7484307af29
Sumet Jutajan <sumet.jutha@gmail.com>
2023-01-25 00:39:23 +0700

    edit files name

--- Python_for_Beginner_Phase_1/ep41_Random_Game.py
+++ Python_for_Beginner_Phase_1/ep41_Random_Game.py
@@ -1,4 +1,4 @@
-### Assignment 16: เกมทายเลขลูกเต๋า
+# Assignment 16: เกมทายเลขลูกเต๋า
 
 ##################################################################################
 # 1,2,3,4,5,6
@@ -19,37 +19,37 @@
 ##################################################################################
 
 ##################################################################################
-## เขียนตามตัวอย่าง
+# เขียนตามตัวอย่าง
 import random
 
-myrandom = random.randrange(1,6+1) # สุ่มตั้งแต่ค่า 1 ถึง สิบ
+myrandom = random.randrange(1, 6+1)  # สุ่มตั้งแต่ค่า 1 ถึง สิบ
 print(myrandom)
-k=1
+k = 1
 print("คุณมีโอกาสทายได้ 3 ครั้ง \n")
-correct=False
+correct = False
 
 while True:
-    number=int(input("ป้อนคำตอบของคุณ = "))
+    number = int(input("ป้อนคำตอบของคุณ = "))
     # correct=(number==myrandom)
-    if number<myrandom:
+    if number < myrandom:
         print("ยังน้อยไปครับ")
-    elif number>myrandom :
+    elif number > myrandom:
         print("ยังมากไปครับ")
-    elif number==myrandom :
+    elif number == myrandom:
         print("ยินดีด้วยครับ คุณตอบถูก รับไปเลย 1 ล้านบาท")
         break
 
-    if number<0 or k==3:
+    if number < 0 or k == 3:
         break
-    k+=1
+    k += 1
     # if not correct:
     #     if (number>myrandom) :
     #         print("ใบนิดนึงว่า ค่าที่ทายมามันน้อยไปครับ")
     #     elif (number<myrandom) :
     #         print("ใบนิดนึงว่า ค่าที่ทายมามันมากไปครับ")
 
-if number!=myrandom :
+if number != myrandom:
     print("เสียใจด้วยนะครัช คุณทายครบ 3 ครั้งแล้ว")
 
-print("เฉลยนะครับ เลขที่ถูกต้องก็คือ ",myrandom," ครับผม")
+print("เฉลยนะครับ เลขที่ถูกต้องก็คือ ", myrandom, " ครับผม")
 ##################################################################################
